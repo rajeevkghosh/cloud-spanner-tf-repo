12a70caa8327ab47c9533f4d799a61e72daf8ada
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-23 20:05:50 +0530

    1

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+
+access_token = var.access_token
+
+}
+
+resource "google_spanner_instance" "example" {
+  config       = "nam3"
+  display_name = "us-dev_abcd_fghi_spanner1"
+  num_nodes    = 2
+  
+  labels = {
+    env                  = "default"
+    application_division = "pci",
+    application_name     = "demo",
+    application_role     = "app",
+    au                   = "0223092",
+    created              = "20211122",
+    data_compliance      = "pci",
+    data_confidentiality = "pub",
+    data_type            = "test",
+    environment          = "dev",
+    gcp_region           = "us",
+    owner                = "hybridenv",
+  }
+}
