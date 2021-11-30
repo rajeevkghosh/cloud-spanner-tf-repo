5d0935e29546b31864d50d1cd683a71459ba392e
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-30 17:18:10 +0530

    5

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ access_token = var.access_token
 }
 
 resource "google_spanner_instance" "example" {
-  config       = "nam3"
-  display_name = "us_dev_abcd_fghi_spanner1"
+  config       = "asia-south1"
+  display_name = "dev_abcd_fghi_spanner1"
   project      = "airline1-sabre-wolverine"
   num_nodes    = 2
   
   labels = {
     env                  = "default"
-    application_division = "pci",
+    #application_division = "pci",
     application_name     = "demo",
     application_role     = "app",
     au                   = "0223092",
